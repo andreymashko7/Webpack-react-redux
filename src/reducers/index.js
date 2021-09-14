9176bb411e58bfee4b859a2b99cbedd866b0be68
andreymashko7 <63639567+andreymashko7@users.noreply.github.com>
2021-09-14 16:11:06 +0300

    Done

---
+++ src/reducers/index.js
@@ -0,0 +1,29 @@
+import { combineReducers } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import reposReducer from "./reposReducer";
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
+
+const rootReducer = combineReducers({
+  repos: reposReducer,
+});
+
+export const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
+
+// import { combineReducers } from "redux";
+// import { createStore, applyMiddleware } from "redux";
+// import { composeWithDevTools } from "redux-devtools-extension";
+// import thunk from "redux-thunk";
+// import reposReducer from "./reposReducer";
+
+// const rootReducer = combineReducers({
+//   repos: reposReducer,
+// });
+
+// export const store = createStore(
+//   rootReducer,
+//   composeWithDevTools(applyMiddleware(thunk))
+// );
